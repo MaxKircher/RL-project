b5abe65b40029efcb9292de76997ac6b29cf9922
MaxKircher <maximilian-kircher@web.de>
2019-01-10 10:58:24 +0100

    new and old plotting

--- MORE/MORE_n_Dim/plot_data.py
+++ MORE/MORE_n_Dim/plot_data.py
@@ -3,18 +3,20 @@ from matplotlib import pyplot as plt
 
 # n = number of parameter
 def plot(rewards, list_of_parameter, n):
-    #rewards = np.array(rewards)
     plt.figure()
-    thetas = get_thetas(list_of_parameter, n)
-    sc = plt.scatter(thetas[:,0], thetas[:,1], c=rewards)
-    plt.colorbar(sc)
-    #print("Shapes:\n", thetas[:,0].shape, thetas[:,1].shape, rewards.shape)
-    #plt.contour(thetas[:,0], thetas[:,1], rewards)
+    if n == 2:
+        thetas = np.array(list_of_parameter)
+        sc = plt.scatter(thetas[:,0], thetas[:,1], c=rewards)
+        plt.colorbar(sc)
+    else:
+        thetas = get_thetas(list_of_parameter, n)
+        plt.scatter(thetas, rewards)
 
     plt.show()
+
 
 def get_thetas(lop, n):
     thetas = []
     for parameter in lop:
-        thetas += [parameter / n]
+        thetas += [sum(parameter) / n]
     return np.array(thetas)
