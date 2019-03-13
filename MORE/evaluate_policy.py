b8343af694922b34e16e336591abda554e7392c5
LenCewa <len13@hotmail.de>
2019-03-13 17:09:23 +0100

    print rewards of RR after merge

--- MORE/evaluate_policy.py
+++ MORE/evaluate_policy.py
@@ -5,24 +5,27 @@ import quanser_robots
 import torch
 import time
 
-input = open("policies/bb_rbf.pkl", "rb")
-#input = open("policies/my_policy_cartpole_cg.pkl", "rb")
+###
+from quanser_robots import GentlyTerminating
+env = GentlyTerminating(gym.make('CartpoleStabRR-v0'))
+###
+
+#input = open("policies/bb_rbf.pkl", "rb")
+input = open("policies/cartpole_rbf100.pkl", "rb")
 data = pickle.load(input)
 #policy = data.get("policy")
 policy = data.get("policy")
 
-env = gym.make('BallBalancerSim-v0')
+#env = gym.make('CartpoleStabShort-v0')
 s = env.reset()
-done = False
-rewards = 0
 
 while not done:
     env.render()
     # a = policy.get_action(s)
     # s, r, d, i = env.step(np.asarray(a))
     a = np.array(policy.get_action(s))
-    print("a", a)
-    print("s", s)
+    # print("a", a)
+    # print("s", s)
     s, r, done, info = env.step(a)
     #time.sleep(.1)
     rewards += r
