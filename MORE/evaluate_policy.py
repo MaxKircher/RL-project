e297bb1181a8fbf0a427f2696980df36736a25a7
LenCewa <len13@hotmail.de>
2019-03-15 10:44:20 +0100

    Data für eval in Sim

--- MORE/evaluate_policy.py
+++ MORE/evaluate_policy.py
@@ -17,19 +17,27 @@ data = pickle.load(input)
 policy = data.get("policy")
 
 env = gym.make('CartpoleStabShort-v0')
-s = env.reset()
+
+
+count = 0
 rewards = 0
-done = False
+lof_rewards = []
 
-while not done:
-    env.render()
-    # a = policy.get_action(s)
-    # s, r, d, i = env.step(np.asarray(a))
-    a = np.array(policy.get_action(s))
-    # print("a", a)
-    # print("s", s)
-    s, r, done, info = env.step(a)
-    #time.sleep(.1)
-    rewards += r
+for steps in range(50):
+    s = env.reset()
+    while count < 1000:
+        # env.render()
+        a = np.array(policy.get_action(s))
+        s, r, done, info = env.step(a)
+        rewards += r
+        count += 1
+        # time.sleep(0.1)
+        if done == True:
+            break
+    lof_rewards += [rewards]
+    rewards = 0
+    count = 0
 
-print("reward of episode: ", rewards)
+print("MORE: " , lof_rewards)
+file = open("EvalSim/cartpole_poldeg2.npy", "wb")
+np.save(file, lof_rewards)
--- TRPO/evaluate_policy.py
+++ TRPO/evaluate_policy.py
@@ -8,28 +8,39 @@ import time
 
 ###
 from quanser_robots import GentlyTerminating
-env = GentlyTerminating(gym.make('BallBalancerRR-v1'))
+#env = GentlyTerminating(gym.make('BallBalancerRR-v1'))
 ###
 
 #input = open("policies/gae.pkl", "rb")
-input = open("policies/my_policy_BallBalancerSim_cont2_2.pkl", "rb")
+input = open("policies/my_policy_cartpole_new_quanser.pkl", "rb")
 data = pickle.load(input)
 #policy = data.get("policy")
 policy = data.get("policy")
 
 #env = gym.make('Pendulum-v2')
-#env = gym.make('CartpoleStabShort-v0')
+env = gym.make('CartpoleStabShort-v0')
 
 
-s = env.reset()
-done = False
+
+count = 0
 rewards = 0
+lof_rewards = []
 
-while not done:
-    env.render()
-    a = policy.model(torch.tensor(s, dtype=torch.float)).detach().numpy()
-    s, r, done, info = env.step(a)
-    rewards += r
-    #time.sleep(0.1)
+for steps in range(50):
+    s = env.reset()
+    while count < 1000:
+        #env.render()
+        a = policy.model(torch.tensor(s, dtype=torch.float)).detach().numpy()
+        s, r, done, info = env.step(a)
+        rewards += r
+        count += 1
+        #time.sleep(0.1)
+        if done == True:
+            break
+    lof_rewards += [rewards]
+    rewards = 0
+    count = 0
 
-print("reward of episode: ", rewards)
+print("TRPO", lof_rewards)
+file = open("EvalSim/TRPO_eval_sim_cartpole.npy", "wb")
+np.save(file, lof_rewards)
